286a9052912419305d27cce637255bfe86202f08
Jack Dahms <jack.dahms@gmail.com>
2021-11-20 18:23:13 -0600

    group lightswitch works

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 Smarthome app designed for Raspberry Pi with touchscreen interface
 
 CSS thanks to `https://codepen.io/kathykato/pen/jzOxgv`
+
+### TODO
+1. Polling is_group_on
--- sunny.py
+++ sunny.py
@@ -1,28 +1,25 @@
 import requests
 
-from flask import Flask, render_template
+from flask import Flask, render_template, current_app, url_for
 
 base_url = 'http://192.168.0.112/api/SGpkOeCn3JCOpW-YqAyE-cFIk7U3w59SKrDCm89G'
 app = Flask(__name__)
-
-def get(endpoint):
-    return requests.get(base_url + endpoint).json()
-
-def put(endpoint, json):
-    return requests.put(base_url + endpoint, json=json).json()[0]
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/toggle/', methods=['PUT'])
+@app.route('/poll')
+def is_group_on():
+    response = requests.get(base_url + '/groups').json()
+    return response['1']['state']
+
+@app.route('/toggle', methods=['PUT'])
 def toggle():
-    response = get('/lights')
-    hue = response['2']['state']['hue']
-    response = put('/lights/2/state', {'hue': 46920 if hue == 8906 else 8906})
-    print(response)
-    print(type(response))
-    return response
+    new_state = not is_group_on()['all_on']
+    json = {'on': new_state}
+    response = requests.put(base_url + '/groups/1/action', json=json)
+    return response.json()[0]
 
 if __name__ == '__main__':
     app.run(port=8888, debug=True) 
